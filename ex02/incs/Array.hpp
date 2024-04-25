71b114e9f29d200e0ebc887e6063ce2f82700fb6
Ahlem NOURI <anouri@student.42.fr>
2024-04-25 11:14:40 +0200

    ex02 ok

--- ex02/incs/Array.hpp
+++ ex02/incs/Array.hpp
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 08:59:53 by anouri            #+#    #+#             */
+/*   Updated: 2024/04/25 11:13:43 by anouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
 #include <exception>
 #include <stdio.h>
+#include <iostream>
 template <typename T>
 class Array
 {
@@ -12,10 +26,12 @@ private:
     unsigned int size;
 public:
     Array<T>(): array(NULL), size(0){}
-    Array<T>(unsigned int n): size(n){
+    Array<T>(unsigned int n): size(n)
+    {
         this->array = new T[size];
     }
-    Array<T>(const Array &src) {
+    Array<T>(const Array &src)
+    {
         size = src.size;
         this->array = new T[size];
         for (int i = 0; i < size; i++)
@@ -23,7 +39,8 @@ public:
             this->array[i] = src.array[i];
         }
     }
-    Array operator=(const Array &rhs){
+    Array operator=(const Array &rhs)
+    {
         if (this != &rhs)
         {
             this->size = rhs.size;
@@ -36,7 +53,48 @@ public:
         }
         return (*this);
     }
-    ~Array(){}
+    ~Array(){delete[] this->array;}
+    unsigned int Sise() const
+    {
+        return (this->size);
+    }
+    // T Access(unsigned int index)
+    // {
+    //     if (index > this->size)
+    //         throw(std::out_of_range("Index out of range"));
+    //     return(this->array[index]);
+    // }
+    // T *getArray()const
+    // {
+    //     return(this->array);
+    // }
+    class outOfBounds : public std::exception
+    {
+        public:
+            using exception ::what;
+            const char *what() const throw()
+            {
+                return ("index is out of bounds");
+            }    
+    };
+
+    
+    T &operator[](unsigned int index) const
+    {
+        if (index >= size)
+            throw(Array::outOfBounds());
+        return (this->array[index]);
+    }
+
 };
+
+// template<typename T>
+// std::ostream &operator<<(std::ostream & o, Array<T> const &rhs)
+// {
+//     for (unsigned int i= 0; i < rhs.Sise(); i++)
+//         o  << rhs.getArray()[i] << std::endl;
+//     return o;
+// }
+
 
 #endif
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,19 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 08:59:59 by anouri            #+#    #+#             */
+/*   Updated: 2024/04/25 11:08:05 by anouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../incs/Array.hpp"
 #include "../../ex01/incs/iter.hpp"
 
-
-
 int main()
 {
-
-    int * a = new int(); 
-    // printrValue(a);
-    std::cout << a[0] << std::endl;
-    std::cout << a[1] << std::endl;
-    std::cout << a[2] << std::endl;
-    std::cout << a[3] << std::endl;
+    Array<int> a(20);
+    for (unsigned int i = 0; i < a.Sise(); i++)
+        std::cout << a[i] << std::endl;
+    std::cout << "*************************************************************" << std::endl;
+    try
+    {
+        std::cout << a[25] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     
-
     return (0);
 }
