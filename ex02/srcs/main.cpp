48bfdc04d8a4f9d0ce5152b7334791fc409ea88f
Ahlem NOURI <anouri@student.42.fr>
2024-05-04 13:54:08 +0200

    final

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,30 +6,9 @@
 /*   By: anouri <anouri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 08:59:59 by anouri            #+#    #+#             */
-/*   Updated: 2024/04/25 11:27:13 by anouri           ###   ########.fr       */
+/*   Updated: 2024/05/04 13:52:36 by anouri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include "../incs/Array.hpp"
-// #include "../../ex01/incs/iter.hpp"
-
-// int main()
-// {
-//     Array<int> a(20);
-//     for (unsigned int i = 0; i < a.Sise(); i++)
-//         std::cout << a[i] << std::endl;
-//     std::cout << "*************************************************************" << std::endl;
-//     try
-//     {
-//         std::cout << a[25] << std::endl;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-    
-//     return (0);
-// }
 
 #include <iostream>
 #include <Array.hpp>
@@ -82,6 +61,11 @@ int main(int, char**)
     {
         numbers[i] = rand();
     }
+    for (size_t i = 0; i < MAX_VAL; i++)
+    {
+        std::cout << "numbers[" << i << "] = " << numbers[i] << std::endl;
+    }
+    
     delete [] mirror;//
     return 0;
 }
